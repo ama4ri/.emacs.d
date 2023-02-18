1c87974bb22a268025f4aee40e6fd796cd14b46f
Alexey Shilov <amauri@amauriMacAir.local>
2023-02-18 14:17:18 +0300

    Change python LSP mode. Maybe it is deb config, just for test

--- init.el
+++ init.el
@@ -17,8 +17,11 @@
       '(magit
         use-package
         lsp-mode
+	lsp-ui
+      ;; company-lsp
         yasnippet
-        org-roam
+      ;; helm
+      ;;  org-roam
 	company ;;will provide auto complete suggestions,
 	flymake ;;will highlight warnings and errors
 	xref ;;can find the definition of a function or variable
@@ -43,23 +46,58 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
-;; enable lsp mode 
-;;              #########################
-;; ##### First you need to install python packages   ####
-;; ##### pip install python-language-server[all]     ####
-;; ##### pip install pyls-black pyls-isort pyls-mypy ####
-;;              #########################
-(use-package lsp-mode
+;; #### CONFIGURE PYTHON LSP MODE  ####
+;; ###################################################################
+;; # First you need to install python packages          	     #
+;; # pip install python-language-server[all] (depricated!!!!!!!)     #
+;; # pip install "python-lsp-server[all]"			     #
+;; # pip install pyls-black pyls-isort pyls-mypy        	     #
+;; ###################################################################
+;;(use-package lsp-mode :ensure t)
+(use-package lsp
+ ;; :commands 'lsp
+  ;; :ensure nil
+  :ensure t
   :config
-  (lsp-register-custom-settings
-   '(("pyls.plugins.pyls_mypy.enabled" t t)
-     ("pyls.plugins.pyls_mypy.live_mode" nil t)
-     ("pyls.plugins.pyls_black.enabled" t t)
-     ("pyls.plugins.pyls_isort.enabled" t t)))
-  :hook
-  ((python-mode . lsp)))
-(use-package lsp-ui
-  :commands lsp-ui-mode)
+  ;; make sure we have lsp-imenu everywhere we have LSP
+  (require 'lsp-imenu)
+  (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)  
+  ;; get lsp-python-enable defined
+  ;; NB: use either projectile-project-root or ffip-get-project-root-directory
+  ;;     or any other function that can be used to find the root directory of a project
+  (lsp-define-stdio-client lsp-python "python"
+                           #'projectile-project-root
+                           '("pyls"))
+
+  ;; make sure this is activated when python-mode is activated
+  ;; lsp-python-enable is created by macro above 
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (lsp-python-enable)))
+
+  ;; lsp extras
+  (use-package lsp-ui
+    :ensure t
+    :config
+    (setq lsp-ui-sideline-ignore-duplicate t)
+    (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+;; NB: only required if you prefer flake8 instead of the default
+  ;; send pyls config via lsp-after-initialize-hook -- harmless for
+  ;; other servers due to pyls key, but would prefer only sending this
+  ;; when pyls gets initialised (:initialize function in
+  ;; lsp-define-stdio-client is invoked too early (before server
+  ;; start)) -- cpbotha
+
+ 
+   (defun lsp-set-cfg ()
+    (let ((lsp-cfg `(:pyls (:configurationSources ("flake8")))))
+      ;; TODO: check lsp--cur-workspace here to decide per server / project
+      (lsp--set-configuration lsp-cfg)))
+
+   (add-hook 'lsp-after-initialize-hook 'lsp-set-cfg)) 
+
+;; ########### END LSP CONFIG  ###################
 
 ;; Show all documenttion
 (setq lsp-eldoc-render-all t)
@@ -97,40 +135,44 @@
 ;; #########################
 ;; HELM - incremental search 
 ;; #########################
-(require 'helm)
 
-(setq-default helm-M-x-fuzzy-match t)
-(global-set-key "\C-x\C-m" 'helm-M-x)
-(global-set-key "\C-c\C-m" 'helm-M-x)
-(define-key evil-ex-map "x" 'helm-M-x)
+;;(require 'helm)
 
-(define-key evil-ex-map "b " 'helm-mini)
-(define-key evil-ex-map "e" 'helm-find-files)
+;;(setq-default helm-M-x-fuzzy-match t)
+;;(global-set-key "\C-x\C-m" 'helm-M-x)
+;;(global-set-key "\C-c\C-m" 'helm-M-x)
+;;(define-key evil-ex-map "x" 'helm-M-x)
+
+;;(define-key evil-ex-map "b " 'helm-mini)
+;;(define-key evil-ex-map "e" 'helm-find-files)
 
 
-###########################################
-####   ORG ROME  ####
-# Whery useful tool to make notes
-##########################################
+;;###########################################
+;;####   ORG ROME  ####
+;;# Whery useful tool to make notes
+;;##########################################
 
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory (file-truename "˜/RoamNotes"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
-  :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  ;;(setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  ;;(require 'org-roam-protocol))
-###########################################################################
+;;(use-package org-roam
+;;  :ensure t
+;;  :custom
+;;  (org-roam-directory (file-truename "˜/RoamNotes"))
+;;  :bind (("C-c n l" . org-roam-buffer-toggle)
+;;         ("C-c n f" . org-roam-node-find)
+;;         ("C-c n g" . org-roam-graph)
+;;         ("C-c n i" . org-roam-node-insert)
+;;         ("C-c n c" . org-roam-capture)
+;;         ;; Dailies
+;;         ("C-c n j" . org-roam-dailies-capture-today))
+;;  :config
+
+
+;;  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+;;  ;;(setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+;;  (org-roam-db-autosync-mode)
+;;  ;; If using org-roam-protocol
+;;  ;;(require 'org-roam-protocol))
+
+;;###########################################################################
 
 
 ;;(custom-set-variables
